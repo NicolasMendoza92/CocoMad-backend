e70c552c6dcde354f78dea384de83967538768a1
NicoMen <nicomendoza.92@gmail.com>
2021-12-29 13:01:38 +0100

    se graba antes del cambio

--- api/routes/salesRoute.js
+++ api/routes/salesRoute.js
@@ -9,9 +9,9 @@ const { check } = require('express-validator');
 router.post(
     '/',
     [
-        check('buyerData.buyerEmail', 'Agrega un Email Valido').isEmail().isLength({ max: 20 }),
+        check('buyerData.buyerEmail', 'Agrega un Email Valido').isEmail().isLength({ max: 40 }),
         check('buyerData.buyerName', 'Nombre obligatorio').not().isEmpty().isLength({ max: 40 }),
-        check('buyerData.buyerLastName', 'Apellido obligatorio').not().isEmpty().isLength({ max: 30 }),
+        check('buyerData.buyerLastName', 'Apellido obligatorio').not().isEmpty().isLength({ max: 40 }),
         check('buyerShipping.pickUp', 'Elija Opcion').not().isEmpty(),
         check('buyerShipping.buyerCity', 'Ciudad Obligatoria').not().isEmpty(),
         check('buyerShipping.buyerState', 'Estado Obligatorio').not().isEmpty(),
