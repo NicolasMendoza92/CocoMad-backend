064598c993ce05af428fc0235b3bb98fdbe8648c
NicoMen <nicomendoza.92@gmail.com>
2022-03-22 12:11:45 +0100

    se saco el cors

--- api/routes/emailsRoute.js
+++ api/routes/emailsRoute.js
@@ -1,10 +1,9 @@
 // Rutas para emails
 const express = require('express');
-const cors = require('cors')
 const router = express.Router();
 const emailController = require('../controllers/emailController');
 
 // para mandar los datos de la compra que hizo el usuario 
-router.post('/',cors(), emailController.createEmail);
+router.post('/', emailController.createEmail);
 
 module.exports = router;
